41d94e2d8cfc42c8397994b1bc7bcea1aafbeca6
Caetano Neto <caetano.n@gmail.com>
2025-12-29 18:22:48 -0300

    componentizando a barra de rolagem

--- onboarding/src/pages/GestorDashboard.jsx
+++ onboarding/src/pages/GestorDashboard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../components/Header";
-import data from "../data/mockData"
+import data from "../data/mockData";
+import Progress from "../components/Progress";
 
 const GestorDashBoard = () => {
   const team = data.usuarios.filter((u) => u.tipo === "colaborador");
@@ -44,21 +45,7 @@ const GestorDashBoard = () => {
                         {colaborador.papel}
                       </li>
                     </ul>
-                    {/* Progress */}
-                    <div className="mt-3">
-                      <div className="flex justify-between items-center mb-1">
-                        <span className="text-xs font-medium text-gray-600">Progresso</span>
-                        <span className="text-xs font-semibold text-yellow-600">
-                          {Math.round(progress(colaborador) * 100)}%
-                        </span>
-                      </div>
-                      <div className="w-full bg-gray-200 rounded-full h-2">
-                        <div 
-                          className="bg-yellow-500 h-2 rounded-full transition-all"
-                          style={{ width: `${progress(colaborador) * 100}%` }}
-                        ></div>
-                      </div>
-                    </div>
+                    <Progress percentage={progress(colaborador)} />
                   </div>
                 </div>
               </div>
